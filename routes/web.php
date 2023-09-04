063b9f809fb7ac1c7b37971a71d20720dbff2618
Shin-Min-Thant <mshin7587@gmail.com>
2023-09-04 17:29:33 +0630

    is_admin middleware

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+// For Admin Group
+Route::middleware(['auth','is_admin'])->group(function(){});
